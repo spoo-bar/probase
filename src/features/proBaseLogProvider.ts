faab08dc48a47599bab02c867740da5ab37d6b33
Spoorthi S <spoorthi_s@ymail.com>
2019-06-20 13:37:39 +0530

    Changes to push logs dynamically to webview

--- src/features/proBaseLogProvider.ts
+++ src/features/proBaseLogProvider.ts
@@ -6,7 +6,7 @@ import ProBaseError from '../utils/probaseError';
 export default class ProBaseLogProvider {
 
     LogPanel : vscode.WebviewPanel | undefined = undefined;
-    LogInterval : NodeJS.Timer = setInterval(() => this.updateTraceLog(this.LogPanel), 1000000);
+    LogInterval : NodeJS.Timer = setInterval(() => this.updateTraceLog(this.LogPanel), 1000);
     State : vscode.Memento;
 
     constructor(context : vscode.ExtensionContext) {
@@ -15,12 +15,61 @@ export default class ProBaseLogProvider {
             this.LogPanel.reveal(vscode.ViewColumn.Beside);
         }
         else {
-            this.LogPanel = vscode.window.createWebviewPanel('log', 'SQL Log', vscode.ViewColumn.Beside, {});
+            this.LogPanel = vscode.window.createWebviewPanel('log', 'SQL Log', vscode.ViewColumn.Beside, {enableScripts: true});
             this.LogPanel.onDidDispose(() => { clearInterval(this.LogInterval); }, null, context.subscriptions);
         }        
     }
 
     public ShowLog() {
+        this.LogPanel!.webview.html = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>SQL Logs</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            <script>
+                window.addEventListener('message', event => {
+                    const message = event.data;
+
+                    //Checking if element already exists and do nothing if it does
+                    var ele = document.getElementById(message.ID);
+                    if(typeof(ele) != undefined && ele != null)
+                        return;
+
+                    var logItemsDiv = document.getElementById('logItems');
+                    var logItem = document.createElement('div');
+                    logItem.id = message.ID;
+                    logItem.className += 'logItemDiv';
+
+                    var playIconLink = document.createElement('button');
+                    var playIcon = document.createElement('i');
+                    playIcon.className += 'icon fa fa-play';
+                    playIcon.style.color = 'green';
+                    playIconLink.appendChild(playIcon);
+                    logItem.appendChild(playIconLink);
+
+                    var logMessage = document.createTextNode(message.Message);
+                    logItem.appendChild(logMessage);
+                    logItemsDiv.appendChild(logItem);
+                });
+            </script>
+            <style>
+                .logItemDiv {
+                    padding: 15px;
+                }
+                .icon {
+                    padding-right: 10px;
+                }
+            </style>
+        </head>
+        <body>
+            <main>
+                <div id="logItems">
+                </div>
+            </main>
+        </body>
+        </html>`;
         this.updateTraceLog(this.LogPanel);
     }
 
@@ -30,26 +79,9 @@ export default class ProBaseLogProvider {
             if (err)
                 throw new ProBaseError(err.name, err.message);
 
-            let details : string = '';
-            let logs : [] = JSON.parse(content);
-            logs.forEach((element: any, index: Number) => {
+            JSON.parse(content).forEach((element: any, index: Number) => {
                 let log: Log = Object.assign(new Log(), element);
-                details += '<p>' + log.Message + '</p>';
-
-                if(index  === logs.length - 1) {
-                    let html = `<!DOCTYPE html>
-                    <html lang="en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>Cat Coding</title>
-                    </head>
-                    <body>
-                        ${details}
-                    </body>
-                    </html>`;
-                    logPanel!.webview.html = html;
-                }
+                logPanel!.webview.postMessage(log);
             });
         });
     }
