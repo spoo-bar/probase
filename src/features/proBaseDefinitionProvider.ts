04b995a52a22ab9025b363d255b950bca9c20494
Spoorthi S <spoorthi_s@ymail.com>
2019-07-23 17:35:50 +0530

    adding checks to verify sqlops engine exists

--- src/features/proBaseDefinitionProvider.ts
+++ src/features/proBaseDefinitionProvider.ts
@@ -25,48 +25,52 @@ export default class ProBaseDefinitionProvider implements vscode.DefinitionProvi
 
 
     private selectTableNode(selectedWord: string) {
-        sqlops.connection.getCurrentConnection().then(connection => {
-            if (connection) {
-                const dbName: string = connection.options.database;
-                sqlops.objectexplorer.getNode(connection.connectionId).then(serverNode => {
-                    if (serverNode) {
-                        serverNode.getChildren().then(serverNodeChildren => {
-                            if (serverNodeChildren) {
-                                for (var serverNodeChild of serverNodeChildren) {
-                                    if (serverNodeChild.label === "Databases") {
-                                        serverNodeChild.getChildren().then(databaseNodeChildren => {
-                                            if (databaseNodeChildren) {
-                                                for (var databaseNodeChild of databaseNodeChildren) {
-                                                    if (databaseNodeChild.label === dbName) {
-                                                        databaseNodeChild.getChildren().then(databaseChildren => {
-                                                            if (databaseChildren) {
-                                                                for (var databaseChild of databaseChildren) {
-                                                                    if (databaseChild.label === "Tables") {
-                                                                        databaseChild.getChildren().then(tablesChildren => {
-                                                                            if (tablesChildren) {
-                                                                                for (var tablesChild of tablesChildren) {
-                                                                                    const tableName = "dbo." + selectedWord;
-                                                                                    if (tablesChild.label.toLowerCase() === tableName.toLowerCase()) {
-                                                                                        tablesChild.setSelected(true, true);
+        if(sqlops) {
+            if(sqlops.connection) {
+                sqlops.connection.getCurrentConnection().then(connection => {
+                    if (connection) {
+                        const dbName: string = connection.options.database;
+                        sqlops.objectexplorer.getNode(connection.connectionId).then(serverNode => {
+                            if (serverNode) {
+                                serverNode.getChildren().then(serverNodeChildren => {
+                                    if (serverNodeChildren) {
+                                        for (var serverNodeChild of serverNodeChildren) {
+                                            if (serverNodeChild.label === "Databases") {
+                                                serverNodeChild.getChildren().then(databaseNodeChildren => {
+                                                    if (databaseNodeChildren) {
+                                                        for (var databaseNodeChild of databaseNodeChildren) {
+                                                            if (databaseNodeChild.label === dbName) {
+                                                                databaseNodeChild.getChildren().then(databaseChildren => {
+                                                                    if (databaseChildren) {
+                                                                        for (var databaseChild of databaseChildren) {
+                                                                            if (databaseChild.label === "Tables") {
+                                                                                databaseChild.getChildren().then(tablesChildren => {
+                                                                                    if (tablesChildren) {
+                                                                                        for (var tablesChild of tablesChildren) {
+                                                                                            const tableName = "dbo." + selectedWord;
+                                                                                            if (tablesChild.label.toLowerCase() === tableName.toLowerCase()) {
+                                                                                                tablesChild.setSelected(true, true);
+                                                                                            }
+                                                                                        }
                                                                                     }
-                                                                                }
+                                                                                });
                                                                             }
-                                                                        });
+                                                                        }
                                                                     }
-                                                                }
+                                                                });
                                                             }
-                                                        });
+                                                        }
                                                     }
-                                                }
+                                                });
                                             }
-                                        });
+                                        }
                                     }
-                                }
+                                });
                             }
                         });
                     }
                 });
             }
-        });
+        }
     }
 }
